b409c7e6ea9ea83302f37267b4c027c11fce9013
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-09-21 23:19:20 +0300

    create readme

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
 # IcyTower
+
+> This is a platform game set in a tower, where the player's goal is to jump from one "floor" to the next and go as high as possible without falling. The higher the player's character climbs, the faster the tower's floors move downward and the harder the game becomes. By default, the player controls the character using a keyboard.
+
+## Animation
+<p align="center">
+  <img width="100%"  src="./src/assets/icy.gif">
+</p>
+
+
+## Built With
+
+- HTML,
+- CSS,
+- Phaser 3,
+- JavaScript,
+- Netlify (for deployment),
+- Webpack,
+- Jest,
+- Leaderboard API service.
+
+## Live Demo
+
+[Live Demo Link]()
+
+### Setup
+
+-To use this app locally, 
+
+[Download](https://github.com/SafaErden/IcyTower/archive/development.zip) or clone this repo:
+
+- Clone with HTTPS:
+```
+https://github.com/SafaErden/IcyTower.git
+```
+- Clone with SHH:
+```
+git@github.com:SafaErden/IcyTower.git
+```
+- run 
+``` 
+npm install
+```
+ in the directory that you download the source.
+
+- run 
+```
+npm run start
+```
+to play the game locally.
+
+### Usage
+
+-If you click on the Score Board button, you can view the highest 10 scores. To go back, you can click on the Menu button,
+
+-To turn off/on the game music, you can visit the options,
+
+-To play the game you need to type a valid name which has at least one character. Then you can clik on the play button to play the game,
+
+-When youstart the game, the main purpose is to stay above the lowest point of the screen. You can move right and left by using the left and right buttons/arrows, 
+
+-To climb the tower you can use the up arrow/button,
+
+-The more you climb, the faster steps move,
+
+-Each step you reveal is 10 points for you,
+
+-Good luck.
+
+
+## Author
+
+👤 **Safa ERDEN**
+
+- Github: [@SafaErden](https://github.com/SafaErden)
+- Twitter: [@safaerden](https://twitter.com/safaerden)
+- Linkedin: [SafaErden](https://www.linkedin.com/in/safaerden/)
+- Mql5: [safaerden](https://www.mql5.com/en/users/safaerden)
+- Email: [safaerden](mailto:safaerden@gmail.com)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/SafaErden/IcyTower/issues)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+This game is originated and inspired form the game named Ict Tower. You can get detailed information about the original game from [here](https://en.wikipedia.org/wiki/Icy_Tower).
--- src/Scenes/ScoreScene.js
+++ src/Scenes/ScoreScene.js
@@ -4,137 +4,131 @@ import { getScore } from '../modules/score';
 import Button from '../Objects/Button';
 
 const updatePanel = (panel, content) => {
-  const sizer = panel.getElement('panel');
-  const { scene } = panel;
+	const sizer = panel.getElement('panel');
+	const { scene } = panel;
 
-  sizer.clear(true);
-  const lines = content.split('\n');
-  for (let li = 0, lcnt = lines.length; li < lcnt; li += 1) {
-    const words = lines[li].split(' ');
-    for (let wi = 0, wcnt = words.length; wi < wcnt; wi += 1) {
-      sizer.add(
-        scene.add
-          .text(0, 0, words[wi], {
-            fontSize: 30,
-          })
-          .setInteractive()
-          .on('pointerdown', () => {
-            this.scene.print.text = this.text;
-            this.setTint(Phaser.Math.Between(0, 0xffffff));
-          }),
-      );
-    }
-    if (li < lcnt - 1) {
-      sizer.addNewLine();
-    }
-  }
+	sizer.clear(true);
+	const lines = content.split('\n');
+	for (let li = 0, lcnt = lines.length; li < lcnt; li += 1) {
+		const words = lines[li].split(' ');
+		for (let wi = 0, wcnt = words.length; wi < wcnt; wi += 1) {
+			sizer.add(
+				scene.add
+					.text(0, 0, words[wi], {
+						fontSize: 30
+					})
+					.setInteractive()
+					.on('pointerdown', () => {
+						this.scene.print.text = this.text;
+						this.setTint(Phaser.Math.Between(0, 0xffffff));
+					})
+			);
+		}
+		if (li < lcnt - 1) {
+			sizer.addNewLine();
+		}
+	}
 
-  panel.layout();
-  return panel;
+	panel.layout();
+	return panel;
 };
 
 export default class ScoreScene extends Phaser.Scene {
-  constructor() {
-    super('ScoreBoard');
-  }
+	constructor() {
+		super('ScoreBoard');
+	}
 
-  preload() {
-    this.add.image(config.width / 2, config.height / 2, 'background');
-  }
+	preload() {
+		this.add.image(config.width / 2, config.height / 2, 'background');
+	}
 
-  async create() {
-    this.creditsButton = new Button(
-      this,
-      config.width / 2,
-      config.height - 50,
-      'blueButton1',
-      'blueButton2',
-      'Menu',
-      'Title',
-    );
+	async create() {
+		this.creditsButton = new Button(
+			this,
+			config.width / 2,
+			config.height - 50,
+			'blueButton1',
+			'blueButton2',
+			'Menu',
+			'Title'
+		);
 
-    this.COLOR_PRIMARY = 0x305556;
-    this.COLOR_LIGHT = 0x40798e;
-    this.COLOR_DARK = 0x89bac7;
+		this.COLOR_PRIMARY = 0x305556;
+		this.COLOR_LIGHT = 0x40798e;
+		this.COLOR_DARK = 0x89bac7;
 
-    this.text = this.add.text(config.width / 2 - 140, 20, 'GAME OVER!', {
-      fill: '#305556',
-      font: '800 50px monospace',
-      strokeThickness: 2,
-    });
+		const scoreBoard = this.rexUI.add
+			.scrollablePanel({
+				x: config.width / 2,
+				y: config.height / 2,
+				width: 400,
+				height: 600,
 
-    const scoreBoard = this.rexUI.add
-      .scrollablePanel({
-        x: config.width / 2,
-        y: config.height / 2,
-        width: 400,
-        height: 600,
+				background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 30, this.COLOR_PRIMARY),
 
-        background: this.rexUI.add.roundRectangle(0, 0, 2, 2, 30, this.COLOR_PRIMARY),
+				panel: {
+					child: this.rexUI.add.fixWidthSizer({
+						space: {
+							left: 5,
+							right: 5,
+							top: 5,
+							bottom: 5,
+							item: 5,
+							line: 5
+						}
+					}),
 
-        panel: {
-          child: this.rexUI.add.fixWidthSizer({
-            space: {
-              left: 5,
-              right: 5,
-              top: 5,
-              bottom: 5,
-              item: 5,
-              line: 5,
-            },
-          }),
+					mask: {
+						padding: 2
+					}
+				},
 
-          mask: {
-            padding: 2,
-          },
-        },
+				space: {
+					left: 20,
+					right: 20,
+					top: 50,
+					bottom: 50,
+					panel: 10
+				}
+			})
+			.layout();
+		updatePanel(scoreBoard, 'Loading...');
 
-        space: {
-          left: 20,
-          right: 20,
-          top: 50,
-          bottom: 50,
-          panel: 10,
-        },
-      })
-      .layout();
-    updatePanel(scoreBoard, 'Loading...');
+		let result = await getScore();
+		result = result.sort((a, b) => +b.score - +a.score);
+		const answer = {};
+		result.forEach((element) => {
+			if (!answer[element.user]) {
+				answer[element.user] = element.score;
+			} else if (+element.score > +answer[element.user]) {
+				answer[element.user] = element.score;
+			}
+		});
 
-    let result = await getScore();
-    result = result.sort((a, b) => +b.score - +a.score);
-    const answer = {};
-    result.forEach((element) => {
-      if (!answer[element.user]) {
-        answer[element.user] = element.score;
-      } else if (+element.score > +answer[element.user]) {
-        answer[element.user] = element.score;
-      }
-    });
+		let output = 'TOP 10 \n\n';
 
-    let output = 'TOP 10 \n\n';
+		const filteredResult = {};
 
-    const filteredResult = {};
-
-    for (let index = 0; index < 10; index += 1) {
-      const key = Object.keys(answer)[index];
-      filteredResult[key] = answer[key];
-    }
-    /* eslint-disable */
+		for (let index = 0; index < 10; index += 1) {
+			const key = Object.keys(answer)[index];
+			filteredResult[key] = answer[key];
+		}
+		/* eslint-disable */
 		for (const el in filteredResult) {
 			output += `${el} - ${filteredResult[el]} \n`;
 		}
 		/* eslint-enable */
-    updatePanel(scoreBoard, output);
-  }
+		updatePanel(scoreBoard, output);
+	}
 
-  centerButton(gameObject, offset = 0) {
-    const cW = config.width;
-    Phaser.Display.Align.In.Center(
-      gameObject,
-      this.add.zone(cW / 2, config.height / 2 - offset * 100, cW, config.height),
-    );
-  }
-  /* eslint-disable */
+	centerButton(gameObject, offset = 0) {
+		const cW = config.width;
+		Phaser.Display.Align.In.Center(
+			gameObject,
+			this.add.zone(cW / 2, config.height / 2 - offset * 100, cW, config.height)
+		);
+	}
+	/* eslint-disable */
 	centerButtonText(gameText, gameButton) {
 		Phaser.Display.Align.In.Center(gameText, gameButton);
 	}
