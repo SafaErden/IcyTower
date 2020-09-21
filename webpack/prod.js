e638de6ee5e6f38c4b6ba78d3b40d84b96ad8148
SafaErden <safa@DESKTOP-F1CKM19.localdomain>
2020-09-21 23:49:23 +0300

    create dist folder for deployment

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,26 +1,28 @@
 const merge = require('webpack-merge'); //eslint-disable-line
 const TerserPlugin = require('terser-webpack-plugin'); //eslint-disable-line
+const CopyPlugin = require('copy-webpack-plugin');
 const base = require('./base');
 
 module.exports = merge(base, {
-  mode: 'production',
-  output: {
-    filename: 'bundle.min.js',
-  },
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000,
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false,
-          },
-        },
-      }),
-    ],
-  },
+	mode: 'production',
+	output: {
+		filename: 'bundle.min.js'
+	},
+	devtool: false,
+	performance: {
+		maxEntrypointSize: 900000,
+		maxAssetSize: 900000
+	},
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					output: {
+						comments: false
+					}
+				}
+			})
+		]
+	},
+	plugins: [ new CopyPlugin([ { from: './src/assets', to: 'src/assets' } ]) ]
 });
